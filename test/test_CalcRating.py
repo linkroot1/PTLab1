616d726804e4ac247349557065e298d0618dc9ae
linkroot1 <lekha-now@mail.ru>
2023-09-13 14:46:10 +0300

    поправил форматирование

--- test/test_CalcRating.py
+++ test/test_CalcRating.py
@@ -33,7 +33,8 @@ class TestCalcRating:
 
         return data, rating_scores
 
-    def test_init_calc_rating(self, input_data: tuple[DataType, RatingsType]) -> None:
+    def test_init_calc_rating(self, input_data: tuple[DataType,
+    RatingsType]) -> None:
         calc_rating = CalcRating(input_data[0])
         assert input_data[0] == calc_rating.data
 
